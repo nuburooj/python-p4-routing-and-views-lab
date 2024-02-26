b615e510ec21665f5d3da12838b75d746e27a2b6
nuburooj <nuburoojkhattak@gmail.com>
2024-02-25 20:33:24 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -6,3 +6,35 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+@app.route('/')
+def idex():
+    return f"<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<string:route>")
+def print_string(route):
+    print(route)
+    return route
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = f""
+    for n in range(number):
+        count += f"{n}\n"
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == "+":
+        return str(num1 + num2)
+    if operation == "-":
+        return str(num1 - num2)
+    if operation == "*":
+        return str(num1 * num2)
+    if operation == "div":
+        return str(num1 / num2)
+    if operation == "%":
+        return str(num1 % num2)
+    else:
+        return f"Operation not found"
